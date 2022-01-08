e6508defd76ef5b090a02dcd3dda187d24890e4c
unknown <khuongduy354@gmail.com>
2022-01-08 21:33:58 +0700

    scrapeHoc247

--- services/scrapesites/__test__/scrapeHoc247.test.js
+++ services/scrapesites/__test__/scrapeHoc247.test.js
@@ -4,13 +4,15 @@ test("Test scrape results for hoc247.com", () => {
   const testURL =
     "https://hoc247.net/cau-hoi-cam-ung-o-dong-vat-la--qid24113.html";
   explain =
-    "Cảm ứng ở động vật là khả năng tiếp nhận kích thích và phản ứng lại các kích thích từ môi trường sống đảm báo cho sinh vật tồn tại và phát triển. ";
+    "Cảm ứng ở động vật là khả năng tiếp nhận kích thích và phản ứng lại các kích thích từ môi trường sống đảm báo cho sinh vật tồn tại và phát triển.";
   correct = "D";
-  options = {
-    A: "Phản xạ có điều kiện",
-    B: "Phản ứng của cơ thể thông qua hệ thần kinh trả lời lại kích thích bên ngoài hoặc bên trong cơ thể",
-    C: "Phản xạ không điều kiện",
-    D: "Khả năng tiếp nhận kích thích và phản ứng lại các kích thích từ môi trường sống đảm báo cho sinh vật tồn tại và phát triển",
-  };
-  expect(scrapeHoc247(testURL)).tobe({ explain, correct, options });
+  options = [
+    "Phản xạ có điều kiện",
+    "Phản ứng của cơ thể thông qua hệ thần kinh trả lời lại kích thích bên ngoài hoặc bên trong cơ thể",
+    "Phản xạ không điều kiện",
+    "Khả năng tiếp nhận kích thích và phản ứng lại các kích thích từ môi trường sống đảm báo cho sinh vật tồn tại và phát triển",
+  ];
+  scrapeHoc247(testURL).then((data) =>
+    expect(data).toEqual({ correct, options, explain })
+  );
 });
--- services/scrapesites/scrapeHoc247.js
+++ services/scrapesites/scrapeHoc247.js
@@ -1,16 +1,40 @@
-"Cảm ứng ở động vật là khả năng tiếp nhận kích thích và";
 const puppeteer = require("puppeteer");
-
-async function scrapeHoc247(url) {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  await page.waitForSelector("input[value='1']");
-  await (await page.$("input[value='1']")).click();
-
-  await page.waitForSelector(".dstl");
-  const ansList = await page.$(".dstl");
+function sentenceToLetter(sentence) {
+  if (sentence.includes("A")) return "A";
+  if (sentence.includes("B")) return "B";
+  if (sentence.includes("C")) return "C";
+  if (sentence.includes("D")) return "D";
+  return None;
 }
-scrapeHoc247("https://hoc247.net/cau-hoi-cam-ung-o-dong-vat-la--qid24113.html");
+async function scrapeHoc247(url) {
+  try {
+    let options = [];
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(0);
+    await page.goto(url);
+
+    //click to show result
+    await page.waitForSelector("input[value='1']");
+    const button = await page.$("input[value='1']");
+    await button.click();
+
+    //get all options
+    await page.waitForSelector(".dstl > *");
+    const ansList = await page.$$(".dstl > *");
+    for (const ans of ansList) {
+      const li = await ans.$$("span");
+      const option = await li[1].evaluate((el) => el.innerText);
+    }
+
+    // get explain and correct answer
+    const questionAnswer = await page.$$(".loigiai > *");
+    const explain = await questionAnswer[0].evaluate((el) => el.innerText);
+    const _correct = await questionAnswer[1].evaluate((el) => el.innerText);
+    const correct = sentenceToLetter(_correct);
+    return { correct, options, explain };
+  } catch (e) {
+    console.log(e);
+  }
+}
 module.exports = scrapeHoc247;
