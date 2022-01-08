208c4bdd3fe7cd2abd5d4f0d0a549caac645ccfc
unknown <khuongduy354@gmail.com>
2022-01-08 17:27:58 +0700

    abandon screenshot

--- services/scrapesites/scrapeHoc247.js
+++ services/scrapesites/scrapeHoc247.js
@@ -1,9 +1,16 @@
 "Cảm ứng ở động vật là khả năng tiếp nhận kích thích và";
 const puppeteer = require("puppeteer");
-module.exports = async function scrapeHoc247(url) {
+
+async function scrapeHoc247(url) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
+
+  await page.waitForSelector("input[value='1']");
+  await (await page.$("input[value='1']")).click();
+
+  await page.waitForSelector(".dstl");
   const ansList = await page.$(".dstl");
-  await ansList.screenshot("");
-};
+}
+scrapeHoc247("https://hoc247.net/cau-hoi-cam-ung-o-dong-vat-la--qid24113.html");
+module.exports = scrapeHoc247;
