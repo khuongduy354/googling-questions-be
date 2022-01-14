453d5993cb814f45de0365d24059efdc1d63d700
khuongduy354 <khuongduy354@gmail.com>
2022-01-14 20:50:15 +0700

    format controller

--- controllers/formatDocController.js
+++ controllers/formatDocController.js
@@ -1,7 +1,16 @@
-// const { formatDoc } = require("../services/formatDoc/formatDoc");
-// function formatDocController(req, res) {
-//   if (req.files === null) return res.status(400);
-//   const file = req.files.file;
-//   const buffer = file.data;
-// }
-// module.exports = formatDocController;
+const { appendGoogle } = require("../helper/appendGoogle");
+const { formatDoc } = require("../services");
+async function formatDocController(req, res) {
+  try {
+    const fileBuffer = req.files.file.data;
+    const { keyword } = req.body;
+    let questionList = await formatDoc(fileBuffer, keyword).catch((e) => {
+      throw new Error(e);
+    });
+    questionList = questionList.map((question) => appendGoogle(question));
+    res.status(200).json({ questionList });
+  } catch (e) {
+    console.log(e);
+  }
+}
+module.exports = formatDocController;
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 8000;
 
 const scrapeSiteController = require("./controllers/scrapeSiteController");
 const { fileValidation } = require("./middlewares/fileValidation");
+const formatDocController = require("./controllers/formatDocController");
+
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
@@ -13,6 +15,7 @@ app.use(fileUpload());
 app.use(fileValidation);
 
 app.post("/scrape-result", fileValidation, scrapeSiteController);
+app.post("/format-doc", formatDocController);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
